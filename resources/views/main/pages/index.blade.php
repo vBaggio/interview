c3a9f662e7a9e6217b143e29a2dedbb939f7aa4b
Vinicius Baggio <vnc.baggio@gmail.com>
2021-10-29 00:46:50 -0300

    we have a template

--- resources/views/main/pages/index.blade.php
+++ resources/views/main/pages/index.blade.php
@@ -3,8 +3,22 @@
 @section('title', 'Home')
 
 @section('content')
-    <br>
-    <h1>Hello, World!</h1>
+    <header class="py-5 bg-dark" id="half-img">
+        <div class="container px-5">
+            <div class="row gx-5 align-items-center justify-content-center">
+                <div class="col-lg-8 col-xl-7 col-xxl-6">
+                    <div class="my-5 text-center text-xl-start">
+                        <h1 class="display-5 mb-2 text-white">A Bootstrap 5 template for modern businesses</h1>
+                        <p class="lead fw-normal mb-4 text-white-50">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                        <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                            <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Get Started</a>
+                            <a class="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 @endsection
 
 
--- resources/views/main/render.blade.php
+++ resources/views/main/render.blade.php
@@ -3,9 +3,11 @@
 @section('body-content')
     @include('main.components.nav')
 
-    <div class="container">
         @yield('content')
-    </div>
 
     @include('main.components.footer')
 @endsection
+
+@push('custom')
+    <link href="/css/custom.css" rel="stylesheet">
+@endpush
